125e2cfe6bc8de157584b2e54d40a794afe22262
Avani1010-prog <avanipandey642@gmail.com>
2025-11-04 18:24:32 +0530

    URL

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://streamify-backend-1-o6bu.onrender.com/api",
   withCredentials: true,
 });
